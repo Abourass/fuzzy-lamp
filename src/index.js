2d51d8206f5a962d375adfbb8f61590016f78682
Abourass <Abourassa@AssetVal.com>
2021-07-20 17:07:12 -0600

    :art: Only draw when mouse is held ddown

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import NerdsRope from "./brushes/NerdsRope.js";
 import Plants from "./brushes/Plants.js";
 import attachBrush from "./tools/attachBrush.js";
 import clearCanvas from "./tools/clearCanvas.js";
-let animate = false, brush = NerdsRope, loops = 1;
-window.addEventListener('mousemove', (e) => attachBrush(e, brush, animate, loops));
-window.addEventListener('click', clearCanvas);
+let drawing = false, animate = false, brush = NerdsRope, loops = 1;
+window.addEventListener('mousemove', (e) => { if (drawing)
+    attachBrush(e, brush, animate, loops); });
+window.addEventListener('mousedown', () => drawing = true);
+window.addEventListener('mouseup', () => drawing = false);
 window.addEventListener('keyup', (e) => {
     if (e.key === 'a') {
         animate = (animate) ? false : true;
     }
     if (e.key === '+') {
         loops++;
+        console.log(loops);
     }
     if (e.key === '-' && loops > 1) {
         loops--;
+        console.log(loops);
     }
     if (e.key === 'c') {
         clearCanvas();
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,15 @@ import Plants from "./brushes/Plants.js";
 import attachBrush from "./tools/attachBrush.js";
 import clearCanvas from "./tools/clearCanvas.js";
 
-let animate = false, brush = NerdsRope, loops = 1;
+let drawing = false, animate = false, brush = NerdsRope, loops = 1;
 
-window.addEventListener('mousemove', (e: MouseEvent) => attachBrush(e, brush, animate, loops));
-window.addEventListener('click', clearCanvas);
+window.addEventListener('mousemove', (e: MouseEvent) => { if (drawing) attachBrush(e, brush, animate, loops) });
+window.addEventListener('mousedown', () => drawing = true);
+window.addEventListener('mouseup', () => drawing = false);
 window.addEventListener('keyup', (e) => {
    if (e.key === 'a') { animate = (animate) ? false : true; }
-   if (e.key === '+') { loops++; } 
-   if (e.key === '-' && loops > 1) { loops--; }
+   if (e.key === '+') { loops++; console.log(loops); } 
+   if (e.key === '-' && loops > 1) { loops--; console.log(loops); }
    if (e.key === 'c') { clearCanvas(); }
    if (e.key === '1') { brush = NerdsRope; }
    if (e.key === '2') { brush = Plants; }
